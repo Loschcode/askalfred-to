372e93179efa0ce7e386a3eced625be685595bf7
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-02-02 23:33:02 -0300

    nuxt with reversed triangle init

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="default">
+    <div class="container">
+      <div class="row">
+        <nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
+
+<script>
+export default {
+  name: 'Default',
+  props: {
+  }
+}
+</script>
