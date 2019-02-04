4b382508ee01b87a0572add24b624205eb3e9c21
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-02-04 17:38:47 -0300

    setup gridle and basic colors

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="default">
     <div class="container">
+
+      <!-- Buttons top right -->
       <div class="row">
+        <div class="gr-3">
+          How does it work ?
+        </div>
+        <div class="gr-3">
+          About us
+        </div>
+        <div class="gr-3">
+          Ask me anything
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="gr-3">
+          I'm alfred
+        </div>
+        <div class="gr-9">
+
+        </div>
         <nuxt />
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
     <div>
-      No Index
+      No index
     </div>
   </section>
 </template>
