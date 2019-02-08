f8a92b399fd03a05bd31154cf07b35b7515973c6
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-02-08 19:14:21 -0300

    more refactoring of the default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="default">
     <div class="wrap container-fluid">
-      <div class="header-background"></div>
+      <div class="header__background"></div>
       <div class="header">
         <div class="row middle-md">
           <div class="col-md-offset-5 col-md-7 +more-than-md">
             <!-- Buttons top right -->
-            <div class="row middle-md header-top-links">
+            <div class="row middle-md header__top-links">
               <div class="col-md-4">
-                <div class="box header-top-links__link">
+                <div class="box header__top-link">
                   <a href="#">How does it work?</a>
                 </div>
               </div>
               <div class="col-md-4">
-                <div class="box header-top-links__link">
+                <div class="box header__top-link">
                   <a href="#">About us</a>
                 </div>
               </div>
               <div class="col-md-4">
-                <div class="box header-top-links__link">
-                  <span class="header-top-links__link--round">
+                <div class="box header__top-link">
+                  <span class="header__top-link--round">
                     <a href="#">Ask me anything</a>
                   </span>
                 </div>
@@ -30,7 +30,7 @@
 
         <div class="row top-md">
           <div class="col-md-6 col-sm-6 col-xs-12">
-            <div class="box header-introduction">
+            <div class="box header__introduction">
               <h1>I'm Alfred</h1>
               <p>Are you tired of<br />losing time ?</p>
               <ul>
@@ -40,7 +40,7 @@
               </ul>
 
               <a
-                class="call-to-action"
+                class="header__call-to-action"
                 href="#"
               >
                 Ask me anything
@@ -50,7 +50,7 @@
 
           <div class="col-md-6 col-sm-6">
             <div class="box">
-              <div class="header-image +more-than-xs">
+              <div class="header__image +more-than-xs">
                 <img src="/header_image.svg" />
               </div>
             </div>
@@ -61,14 +61,14 @@
 
       <!-- Inside layout -->
       <div class="row">
-        <div class="after-header-background"></div>
+        <div class="header__background-after"></div>
       </div>
 
       <div class="row">
         <nuxt />
       </div>
 
-      <div class="footer-background">
+      <div class="footer__background">
         <div class="row center-xs">
           <div class="col-md-12">
             <div class="footer">
@@ -84,34 +84,37 @@
 <style lang="scss">
 @import "assets/styles/app";
 
-.header-image {
+.header {
+  color: $color-white;
+}
+
+.header__image {
   margin: spacing(10);
   width: spacing(70);
 }
 
-.header-top-links {
+.header__top-links {
   a {
     color: $color-white;
   }
   margin: auto;
   padding-top: spacing(6);
+
+  .header__top-link {
+    text-align: center;
+  }
+
+  .header__top-link--round {
+    @include button-radius("big");
+    padding: spacing(1, 4, 1, 4);
+    border: 1px solid $color-white;
+  }
 }
 
-.header-top-links__link {
-  text-align: center;
-}
-
-.header-top-links__link--round {
-  border-radius: 2em;
-  padding: spacing(1, 4, 1, 4); // 0.4em 1.2em 0.4em 1.2em;
-  border: 1px solid $color-white;
-}
-
-.call-to-action {
+.header__call-to-action {
+  @include button-radius("big");
   margin: -0.5em;
-  border-radius: 2em;
-  padding: 1.3em 4em 1.3em 4em;
-  font-family: $font-alternative;
+  padding: spacing(4, 12, 4, 12);
   font-size: sizing(2);
   text-transform: uppercase;
   background-color: $color-white;
@@ -119,16 +122,17 @@
   font-weight: bold;
 }
 
-.header {
-  color: $color-white;
-}
-
-.header-background {
+.header__background {
   @include background-absolute();
   @include bottom-radius(50%);
   z-index: -1;
   background: $color-blue url("/header_background.svg") no-repeat fixed center;
-  padding-bottom: 33em;
+  padding-bottom: spacing(110);
+
+  @include breakpoint("md") {
+    @include bottom-radius(40%);
+    padding-bottom: spacing(95);
+  }
 
   @include breakpoint("sm") {
     @include bottom-radius(40%);
@@ -139,12 +143,12 @@
   }
 }
 
-.after-header-background {
+.header__background-spacing {
   @include breakpoint("lg") {
     padding-top: spacing(5);
   }
   @include breakpoint("md") {
-    padding-top: spacing(18);
+    padding-top: spacing(8);
   }
   @include breakpoint("sm") {
     padding-top: spacing(8);
@@ -154,7 +158,7 @@
   }
 }
 
-.header-introduction {
+.header__introduction {
   margin-left: spacing(15);
   @include breakpoint("md") {
     margin-left: spacing(10);
@@ -189,7 +193,7 @@
   }
 }
 
-.footer-background {
+.footer__background {
   @include background-absolute();
   @include top-radius(50%);
   bottom: 0;
