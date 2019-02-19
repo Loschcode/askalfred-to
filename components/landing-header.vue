43402a370626c50fc0f6c516d166200089a9239a
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-02-19 17:31:03 -0300

    modal kind of working

--- components/landing-header.vue
+++ components/landing-header.vue
@@ -39,10 +39,15 @@
 
           <span class="landing-header__call-to-action">
             <a
-              v-on:click="startModal()"
+              v-on:click="$refs.askModal.open()"
               href="#"
             >Ask me anything</a>
           </span>
+
+          <modal ref="askModal">
+            This is some real content
+          </modal>
+
         </div>
       </div>
 
@@ -163,88 +168,22 @@
 </style>
 
 <script>
-import TingleModal from '../components/tingle-modal'
+import Modal from '../components/modal'
 
 export default {
   name: 'LandingHeader',
   methods: {
-    startModal () {
-      if (process.client) {
-        var vm = this
-        var tingle = require('tingle.js');
+  },
 
-        var modal = new tingle.modal({
-          footer: false,
-          stickyFooter: false,
-          closeMethods: ['overlay', 'button', 'escape'],
-          closeLabel: "Close",
-          // cssClass: ['custom-class-1', 'custom-class-2'],
-          onOpen: function () {
-            console.log('modal open');
-          },
-          onClose: function () {
-            console.log('modal closed');
-          },
-          beforeClose: function () {
-            // here's goes some logic
-            // e.g. save content before closing the modal
-            return true; // close the modal
-            return false; // nothing happens
-          }
-        });
-
-        // // set content
-        modal.setContent('<h1>here\'s some content</h1>');
-
-        // // add a button
-        // modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
-        //     // here goes some logic
-        //     modal.close();
-        // });
-
-        // // add another button
-        // modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
-        //     // here goes some logic
-        //     modal.close();
-        // });
-
-        // // open modal
-        modal.open();
-
-        // // close modal
-        // modal.close();
-
-        //   this.tingle = new tingle.modal({
-        //     footer: vm.buttons.length > 0,
-        //     onOpen () {
-        //       if (typeof vm.onopen === 'function') {
-        //         vm.onopen.call(vm)
-        //       }
-        //     },
-        //     onBeforeClose () {
-        //       if (typeof vm.onbeforeclose === 'function') {
-        //         vm.onbeforeclose.call(vm)
-        //       }
-        //     },
-        //     onClose () {
-        //       if (typeof vm.onclose === 'function') {
-        //         vm.onclose.call(vm)
-        //         vm.tingle.destroy()
-        //       }
-        //     }
-        //   })
-
-        //   this.tingle.setContent(this.$el)
-
-        //   this.buttons.forEach(button => {
-        //     this.tingle.addFooterBtn(button.text, button.class || 'tingle-btn', button.action)
-        //   })
-        // }
-      }
+  data () {
+    return {
     }
   },
+
   props: {
-    // TingleModal
+  },
+  components: {
+    Modal
   }
 }
 </script>
---
+++ components/modal.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="modal">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.modal-placeholder {
+  display: none;
+}
+</style>
+
+<script>
+
+export default {
+  name: 'Modal',
+  props: {
+  },
+
+  data () {
+    return {
+      tingle: null,
+    }
+  },
+
+  methods: {
+    open () {
+      this.makeModal();
+    },
+
+    makeModal () {
+      var vm = this
+      var tingle = require('tingle.js');
+
+      var modal = new tingle.modal({
+        footer: false,
+        stickyFooter: false,
+        closeMethods: ['overlay', 'button', 'escape'],
+        closeLabel: "Close",
+        // cssClass: ['custom-class-1', 'custom-class-2'],
+        onOpen: function () {
+          console.log('modal open');
+        },
+        onClose: function () {
+          console.log('modal closed');
+        },
+        beforeClose: function () {
+          // here's goes some logic
+          // e.g. save content before closing the modal
+          return true; // close the modal
+          return false; // nothing happens
+        }
+      });
+
+      modal.setContent(vm.$el)
+      modal.open();
+    }
+  },
+
+  // watch: {
+  //   open (val) {
+  //     if (val === true) {
+  //       this.makeModal();
+  //       // this.$nextTick(() => this.tingle.open())
+  //       return
+  //     }
+
+  //     this.tingle.close()
+  //   }
+  // },
+
+  mounted () {
+    if (process.client) {
+
+      // WORKING
+      // var vm = this
+      // var tingle = require('tingle.js');
+
+      // var modal = new tingle.modal({
+      //   footer: false,
+      //   stickyFooter: false,
+      //   closeMethods: ['overlay', 'button', 'escape'],
+      //   closeLabel: "Close",
+      //   // cssClass: ['custom-class-1', 'custom-class-2'],
+      //   onOpen: function () {
+      //     console.log('modal open');
+      //   },
+      //   onClose: function () {
+      //     console.log('modal closed');
+      //   },
+      //   beforeClose: function () {
+      //     // here's goes some logic
+      //     // e.g. save content before closing the modal
+      //     return true; // close the modal
+      //     return false; // nothing happens
+      //   }
+      // });
+
+      // modal.setContent(vm.$el)
+      // modal.open();
+      // DONE WORKING
+
+      // // add a button
+      // modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
+      //     // here goes some logic
+      //     modal.close();
+      // });
+
+      // // add another button
+      // modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
+      //     // here goes some logic
+      //     modal.close();
+      // });
+
+      // // open modal
+
+      // // close modal
+      // modal.close();
+
+      //   this.tingle = new tingle.modal({
+      //     footer: vm.buttons.length > 0,
+      //     onOpen () {
+      //       if (typeof vm.onopen === 'function') {
+      //         vm.onopen.call(vm)
+      //       }
+      //     },
+      //     onBeforeClose () {
+      //       if (typeof vm.onbeforeclose === 'function') {
+      //         vm.onbeforeclose.call(vm)
+      //       }
+      //     },
+      //     onClose () {
+      //       if (typeof vm.onclose === 'function') {
+      //         vm.onclose.call(vm)
+      //         vm.tingle.destroy()
+      //       }
+      //     }
+      //   })
+
+      //   this.tingle.setContent(this.$el)
+
+      //   this.buttons.forEach(button => {
+      //     this.tingle.addFooterBtn(button.text, button.class || 'tingle-btn', button.action)
+      //   })
+      // }
+    }
+
+    // if (process.client) {
+    //   var vm = this
+    //   var tingle = require('tingle.js');
+
+    //   this.tingle = new tingle.modal({
+    //     footer: vm.buttons.length > 0,
+    //     onOpen () {
+    //       if (typeof vm.onopen === 'function') {
+    //         vm.onopen.call(vm)
+    //       }
+    //     },
+    //     onBeforeClose () {
+    //       if (typeof vm.onbeforeclose === 'function') {
+    //         vm.onbeforeclose.call(vm)
+    //       }
+    //     },
+    //     onClose () {
+    //       if (typeof vm.onclose === 'function') {
+    //         vm.onclose.call(vm)
+    //         vm.tingle.destroy()
+    //       }
+    //     }
+    //   })
+
+    //   this.tingle.setContent(this.$el)
+
+    //   this.buttons.forEach(button => {
+    //     this.tingle.addFooterBtn(button.text, button.class || 'tingle-btn', button.action)
+    //   })
+    // }
+  }
+}
+</script>
