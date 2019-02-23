0ef5b322563a16fe976f82bd1e682f8ab1839e6e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-02-23 18:47:46 -0300

    random example done from library

--- services/campaign-service.js
+++ services/campaign-service.js
@@ -7,17 +7,39 @@ class CampaignService {
   constructor(vm, campaign) {
     this.vm = vm;
     this.campaign = campaign;
+    this.mapSelection = this.random(this.map());
   }
 
   /**
    * We take care of the connection
    */
   question() {
-    return "I want to organize a romantic dinner with my girlfriend.";
+    return this.mapSelection.question;
   }
 
   answer() {
-    return "Sure, what budget do you have?";
+    return this.mapSelection.answer;
+  }
+
+  map() {
+    return [
+      {
+        question: "I want to organize a romantic dinner with my girlfriend",
+        answer: "Sure, what budget do you have?"
+      },
+      {
+        question: "I'm lost and need to find my way to the hotel",
+        answer: "No worry, I'll help you out right now."
+      },
+      {
+        question: "I need you to call this place for me about my package",
+        answer: "This will be done today."
+      }
+    ];
+  }
+
+  random(array) {
+    return array[(Math.random() * array.length) | 0];
   }
 
   /**
