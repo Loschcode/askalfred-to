acb642aaa2aa3bf99eb009f1eed80e88249616c8
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-02-28 18:21:41 -0300

    better SEO tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: "AskAlfred",
+    title: "I'm Alfred, ask me anything.",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
