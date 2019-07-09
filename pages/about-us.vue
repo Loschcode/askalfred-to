27c970cd092c3f5272b84e0b80307fdf84ffc6c3
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-07-09 17:19:50 -0400

    spelling mistake

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -6,11 +6,11 @@
           <div class="page__block">
             <h3>Who are we?</h3>
             <p>AskAlfred is a startup founded by a small group of entrepreneurs who grew tired of wasting time on some daily tasks.</p>
-            <p>The world is changing and technologies are evolving, you see tech companies popping up all over the place to ease your life in many ways; transportations, hosting, insurances... All that was reserved to few people before it got democratised.</p>
+            <p>The world is changing and technologies are evolving, you see tech companies popping up all over the place to ease your life in many ways; transportations, hosting, insurances... All that was reserved for a few people before it got democratised.</p>
             <p>There's something missing somewhere. This thing is Alfred. A 24/24H personal assistant — or a concierge as you could call it — to help you out whenever you want, for whatever reason.</p>
             <p>The idea united us, with our knowledge in various fields, we could make this dream a reality.</p>
             <h3>Our [short] story</h3>
-            <p>We started to build up Alfred as personal assistant in early 2019. The idea behind it was to design a system where anyone would be able to invoke Alfred to help him out for anything.</p>
+            <p>We started to build up Alfred as personal assistant in early 2019. The idea behind it was to design a system where anyone would be able to invoke Alfred to help out for anything.</p>
             <p>Starting from this statement, we tried to cross-check all possibilities, tried things, rolled back and tried again to make all this work smoothly.</p>
             <p>We now entered the testing phase. The service is in open-beta version.</p>
             <h3>We are hiring!</h3>
